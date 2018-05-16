4bf8cd114a0785b38d82d3f09703087aff1b5b46
Ioana Vladau <ioan0414@stud.kea.dk>
2018-05-16 23:55:47 +0200

    js with descriptive comments

--- w20thursday/mysolution/script.js
+++ w20thursday/mysolution/script.js
@@ -5,17 +5,11 @@ const listItems = Array.from(listItemsNodes);
 const body = document.documentElement;
 // listen for clicks on the color-selectors
 listItems.forEach((e)=>{
+    // find the element that was clicked
     e.addEventListener("click", ()=>{
+        // find the color from that element
         console.log(`Base color: ${e.dataset.basecolorValue}`)
+        // set the css variable --basecolor 
         body.style.setProperty('--basecolor', e.dataset.basecolorValue);
     })
-})
-
-
-// when one is clicked:
-
-    // find the element that was clicked
-
-    // find the color from that element
-
-    // set the css variable --basecolor 
+});
