183ebbbfdd89718b1a4854e2e838d992f9a80638
Ioana Vladau <ioan0414@stud.kea.dk>
2018-06-18 16:57:37 +0200

    tried some more, somehow succeeded?

--- w21wednesday/mysolution/3dplanes.js
+++ w21wednesday/mysolution/3dplanes.js
@@ -2,7 +2,6 @@
 
 document.querySelector("#stage").addEventListener("mousemove", mouseMove);
 
-
 function mouseMove(e) {
     // Find the x and y coordinates of the mouse, relative to the stage (upper left corner is 0,0)
     let xCoord = e.clientX;
@@ -18,10 +17,16 @@ function mouseMove(e) {
     let distX = centerX-xCoord;
     let distY = centerY-yCoord;
     console.log(`X: ${distX}, Y: ${distY}`)
+    
     // Calculate the relativistic distance, using this formula
 
     let relativeX = ( centerX + distX ) / centerX;
-    console.log(`relativeX: ${relativeX}`)
+    console.log(`relativeX distance: ${relativeX}`)
+
+    let relativeY = ( centerY + distY ) / centerY;
+    console.log(`relativeY distance: ${relativeX}`)
+// These 2 values relativeX and Y range in my project from 0 to 2, not from -1 to 1
+
 
     // This gives you a value from -1 when the mouse is at the very left, 
     //                           to 0 when the mouse is in the center, 
@@ -38,6 +43,23 @@ function mouseMove(e) {
     // 3. Let the foreground move opposite the mouse, but not as much as the mouse.
     //  you are going to need a factor, a value that it max should be moved by. Then try:
     //        position = centerX - factor * relativeX
+
+
+    let factor = 100;
+    let positionX = centerX - factor * relativeX;
+    document.querySelector("#foreground").style.left = positionX + "px";
+
+    let positionY = centerY - factor * relativeY;
+    document.querySelector("#foreground").style.top = positionY+"px";
+
+    console.log("positionX: " + positionX);
+    console.log("positionY: " + positionY);
+
+    let factor2 = -100;
+    let positionX2 = centerX*(-0.005) - factor2 * relativeX*2;
+    let positionY2 = centerY*(-0.008) - factor2 * relativeY*2;
+    document.querySelector("#background").style.left = positionX2+"px";
+    document.querySelector("#background").style.top = positionY2+"px";
 
     // When done, get it working for the vertical (y) movement as well - use the same formulas, but with different values almost everywhere
 
